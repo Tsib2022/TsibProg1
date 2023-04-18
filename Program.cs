1ae836ff69dbfc4a6528939785bf9e0c9b0aff9b
TheSecondTsib <Tsib2016@gmail.com>
2023-04-18 13:02:23 +0300

    Первая готова

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int a, b, max, min;
+a = 5;
+b = 7;
+if (a > b)
+{
+    max =a;
+    min =b;
+}
+else
+{
+    max =b;
+    min =a;
+}
+
+Console.WriteLine("Минимальное = ");
+Console.WriteLine(min);
+Console.WriteLine("Максимальное =");
+Console.WriteLine(max);
